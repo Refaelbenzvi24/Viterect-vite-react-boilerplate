98a2d591daa944606d375ab17c187253c82a4947
Refael <refaelbenzvi24@gmail.com>
2022-02-26 18:47:41 +0200

    jest coverage fixed

--- tests/unit/modules/ApiUrlService.ts
+++ tests/unit/modules/ApiUrlService.ts
@@ -11,9 +11,36 @@ describe('ApiUrlService', () => {
 		apiCurrentVersion: '/v1'
 	}
 
+	it('should be defined', () => {
+		expect(ApiUrlService)
+			.toBeDefined()
+	})
+
+	it('should have a apiFullRootUrl property', () => {
+		stub = new ApiUrlService(stubData)
+
+		expect(stub.apiFullRootUrl)
+			.toBe('https://bing-news-search1.p.rapidapi.com/v1')
+	})
+
+	it('should add version to apiFullRootUrl', () => {
+		stub = new ApiUrlService({
+			apiRootUrl: 'https://bing-news-search1.p.rapidapi.com'
+		})
+		expect(stub.apiFullRootUrl)
+			.toBe('https://bing-news-search1.p.rapidapi.com')
+
+		stub = new ApiUrlService({
+			apiRootUrl:        'https://bing-news-search1.p.rapidapi.com',
+			apiCurrentVersion: '/v1'
+		})
+		expect(stub.apiFullRootUrl)
+			.toBe('https://bing-news-search1.p.rapidapi.com/v1')
+	})
+
 	beforeEach(() => {
 		stub            = new ApiUrlService(stubData)
-		urlParamsObject = { count: 20, search: '' }
+		urlParamsObject = { count: 20, search: 'someSearchString' }
 		urlParamsString = stub.buildUrlParams(urlParamsObject)
 	})
 
@@ -38,14 +65,30 @@ describe('ApiUrlService', () => {
 
 		it("should ignore empty params", () => {
 			const expectedResult = '?count=20'
+			urlParamsObject      = { count: 20, search: "" }
 
 			expect(stub.buildUrlParams(urlParamsObject)).toBe(expectedResult)
 		})
 
-		it('should ignore when sending string in addition to object', () => {
+		it('should ignore when sending empty params and a string', () => {
 			const expectedResult = '?count=20&count=20'
+			urlParamsObject      = { count: 20, search: "" }
+			urlParamsString      = stub.buildUrlParams(urlParamsObject)
 
 			expect(stub.buildUrlParams(urlParamsObject, urlParamsString)).toBe(expectedResult)
+		})
+
+		it('should return encodedUrlParams when empty obj sent', () => {
+			const expectedResult = '?count=20&search=someSearchString'
+			urlParamsObject      = {}
+
+			expect(stub.buildUrlParams(urlParamsObject, urlParamsString)).toBe(expectedResult)
+		})
+
+		it('should return empty string when sending empty params', () => {
+			const expectedResult = ''
+
+			expect(stub.buildUrlParams({})).toBe(expectedResult)
 		})
 	})
 })

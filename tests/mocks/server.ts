c2726bfccd9f554fb8eaff6d287424bac0d2cc9a
Refael <refaelbenzvi24@gmail.com>
2022-02-27 08:57:25 +0200

    removed msw

--- tests/mocks/server.ts
+++
@@ -1,6 +0,0 @@
-import { setupWorker } from 'msw'
-import coinsRanking from './coinsRanking'
-import bingNews from "./bingNews"
-
-const server = setupWorker(...coinsRanking, ...bingNews)
-export default server
